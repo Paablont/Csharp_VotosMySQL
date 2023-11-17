6e51d22bedf118e6165b92c1d526dd08abf008c2
Pablo <paablont@gmail.com>
2023-11-17 12:49:41 +0100

    Campo modificado en el UPDATE (era lo que daba fallo al actualizar datos)

--- Csharp_VotosMySQL/ModelView/PartyModelView.cs
+++ Csharp_VotosMySQL/ModelView/PartyModelView.cs
@@ -113,7 +113,7 @@ namespace Csharp_VotosMySQL.ModelView
 
         public void UpdateParty()
         {
-            String SQL = $"UPDATE partido SET nombre = '{name}', acronimo = '{acronym}',nombreRepresentante = '{presidentName}'" +
+            String SQL = $"UPDATE partido SET nombre = '{name}', acronimo = '{acronym}',nombrePresidente = '{presidentName}'" +
                 $",votos = '{votesParty}',escanios = '{seatCount}' WHERE nombre = '{name}';";
             MySQLDataComponent.ExecuteNonQuery(SQL, cnstr);
         }
